63d3b1f3d677ec7171669f79ced04be742d736b9
Louis TOUSSAINT <louistoussaint98@gmail.com>
2021-02-02 20:05:47 +0100

    ISSUE 79: Create a more readable aws_route53_zone output

--- pkg/resource/aws/aws_route53_zone_test.go
+++ pkg/resource/aws/aws_route53_zone_test.go
@@ -12,11 +12,19 @@ func TestAwsRoute53Zone_String(t *testing.T) {
 		zone AwsRoute53Zone
 		want string
 	}{
-		{name: "",
+		{name: "test route53 zone stringer with name and id",
 			zone: AwsRoute53Zone{
 				Name: aws.String("example.com"),
+				Id:   "ZOS30SFDAFTU9__github-challenge-cloudskiff.cloudskiff.com_TXT",
 			},
-			want: "example.com",
+			want: "example.com (Id: ZOS30SFDAFTU9__github-challenge-cloudskiff.cloudskiff.com_TXT)",
+		},
+		{name: "test route53 zone stringer without name",
+			zone: AwsRoute53Zone{
+				Name: nil,
+				Id:   "ZOS30SFDAFTU9__github-challenge-cloudskiff.cloudskiff.com_TXT",
+			},
+			want: "ZOS30SFDAFTU9__github-challenge-cloudskiff.cloudskiff.com_TXT",
 		},
 	}
 	for _, tt := range tests {
